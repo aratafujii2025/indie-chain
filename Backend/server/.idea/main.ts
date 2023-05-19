2a22c300b3ec01fbdbdfcd3a2e8f5df38fa9402a
Jason Silva <littlejsilva@gmail.com>
2023-05-19 11:05:51 -0400

    documentation

---
+++ Backend/server/.idea/main.ts
@@ -0,0 +1,14 @@
+const http = require('http')
+
+
+const port = 8000
+const host = 'localhost'
+const requestListener = function (req, res) {
+    res.writeHead(200);
+    res.end("My first server!");
+};
+
+
+const server = http.createServer(requestListener)
+
+server.listen(port,host,()=>{console.log('server is running')})
