c5c2c09f77eb4bcb14729833391cec7e02528174
Jason Silva <littlejsilva@gmail.com>
2023-05-22 21:07:55 -0400

    database intros

--- Backend/server/.idea/main.ts
+++ Backend/server/.idea/main.ts
@@ -1,14 +1,15 @@
 const http = require('http')
-
-
 const port = 8000
 const host = 'localhost'
+
+//A sample event listener
 const requestListener = function (req, res) {
     res.writeHead(200);
     res.end("My first server!");
 };
 
 
+// This is the syntax for adding event listeners, here requestListener is an event listener.
 const server = http.createServer(requestListener)
 
 server.listen(port,host,()=>{console.log('server is running')})
