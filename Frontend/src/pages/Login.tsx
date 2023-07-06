8c9a29980b3fbea5d2cd2d302a21a8ff1e12c4fd
Naomi LeDell <naomi_ledell@brown.edu>
2023-07-07 06:46:59 +0900

    more work

--- Frontend/src/pages/Login.tsx
+++ Frontend/src/pages/Login.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 (callback:any) => {<input type="text" /> }
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 
 function LoginPage() {
@@ -30,32 +32,31 @@ function LoginPage() {
         <h5 style={{fontSize:"50px"}}></h5>
         <h2 className="black-italic-text">Welcome Back</h2>
         <form onSubmit={handleSubmit}>
-          <div>
-            <label className="userpass">Username:</label>
-            <input
-              type="text"
-              className="white-input"
-              id="username"
-              value={username}
-              onChange={handleUsernameChange}
-              color="white"
-            />
+            <div className="input-container">
+              <FontAwesomeIcon icon={faEnvelope} className="icon" />
+              <input
+                type="text"
+                id="username"
+                value={username}
+                onChange={handleUsernameChange}
+                className="input-field"
+              />
+              <hr className={`underline ${username ? 'filled' : ''}`} />
           </div>
-          <div>
-            <label className="userpass">Password:</label>
+          <div className="input-container">
+            <FontAwesomeIcon icon={faUser} className="icon" />
             <input
               type="password"
-              className="white-input"
               id="password"
               value={password}
               onChange={handlePasswordChange}
+              className="input-field"
             />
+            <hr className={`underline ${username ? 'filled' : ''}`} />
           </div>
           <button type="submit">Login</button>
-          <div className="or-divider">
-            <hr />
-            <span>or</span>
-            <hr />
+          <div className="divider">
+            <span className="or-text">or</span>
           </div>
           <button className="googlelogin">Sign in with Google</button>
         </form>
