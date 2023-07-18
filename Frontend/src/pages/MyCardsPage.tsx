c508f3eeadd59ffe0afe6a79e856ca2ee9c2d29e
Naomi LeDell <naomi_ledell@brown.edu>
2023-07-19 05:15:18 +0900

    cant get the bottom containers to line up

--- Frontend/src/pages/MyCardsPage.tsx
+++ Frontend/src/pages/MyCardsPage.tsx
@@ -1,25 +1,38 @@
 import React from 'react';
+import { FaSearch } from 'react-icons/fa';
 
 const MyCardsPage = () => {
   return (
     <div className="bg-cream h-screen">
       <div className="flex items-center justify-between px-6 py-4">
-        <div className="flex items-center">
+        <div className="flex items-center flex-grow">
           <div className="w-10 h-10 bg-white rounded-full"></div>
           <button className="ml-4">My Cards</button>
           <button className="ml-4">Following</button>
-          <input
-            type="text"
-            className="ml-4 border border-gray-400 rounded py-1 px-2 search-bar"
-            placeholder="Search..."
-          />
+          <div className="search-bar">
+            <FaSearch className="search-icon" />
+            <input
+              type="text"
+              className="search-input"
+              placeholder="Search..."
+            />
+          </div>
         </div>
         <div className="flex items-center">
-          <button className="ml-4">Discover</button>
-          <button className="ml-4">Account</button>
-          <button className="ml-4">Logout</button>
+          <button className="ml-4 flex-shrink-0">Discover</button>
+          <button className="ml-4 flex-shrink-0">Account</button>
+          <button className="ml-4 flex-shrink-0">Logout</button>
         </div>
       </div>
+      <div className="smalltext">My Cards</div>
+      <div className="bigtext">Manage and Browse through Artist Cards</div>
+      <div className="container"></div>
+      <div className="white-container"></div>
+      <div className="wflex"></div>
+        <div className="egg-container"></div>
+        <div className="purple-container"></div>
+      
+          
     </div>
   );
 };
