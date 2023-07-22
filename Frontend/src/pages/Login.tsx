bd21c5a10563a9c2258549356202147705f7dd12
Jason Silva <littlejsilva@gmail.com>
2023-07-22 10:07:47 -0400

    email api

--- Frontend/src/pages/Login.tsx
+++ Frontend/src/pages/Login.tsx
@@ -1,57 +1,84 @@
-import React, { useState } from 'react';
-(callback:any) => {<input type="text" /> }
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faKey } from '@fortawesome/free-solid-svg-icons';
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
-import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faKey } from "@fortawesome/free-solid-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
+import axios from "axios";
+
+const API_URL = "http://localhost:3000/";
 
 function LoginPage() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-  const handleUsernameChange = (event: { target: { value: React.SetStateAction<string>; }; }) => {
+  const handleUsernameChange = (event: {
+    target: { value: React.SetStateAction<string> };
+  }) => {
     setUsername(event.target.value);
   };
 
-  const handlePasswordChange = (event: { target: { value: React.SetStateAction<string>; }; }) => {
+  const handlePasswordChange = (event: {
+    target: { value: React.SetStateAction<string> };
+  }) => {
     setPassword(event.target.value);
   };
 
-  const handleSubmit = (event: React.FormEvent) => {
+  const handleLogin = (event: React.FormEvent) => {
     event.preventDefault();
+    axios
+      .post(API_URL + "login", { username: username, password: password })
+      .then((result) => console.log("success"));
+    setUsername("");
+    setPassword("");
+  };
+
+  const handleRegister = (event: React.FormEvent) => {
+    event.preventDefault();
+    console.log(event.target);
     // Perform login logic here with username and password
-    console.log('Login submitted:', username, password);
+    axios
+      .post(API_URL + "register", {
+        username: username,
+        password: password,
+      })
+      .then((value) => {
+        console.log("success");
+      })
+      .catch((err) => {
+        console.log("A user with that name already exists");
+      });
+    console.log("Login submitted:", username, password);
     // Clear username and password fields after submission
-    setUsername('');
-    setPassword('');
+    setUsername("");
+    setPassword("");
   };
 
   return (
     <div className="login-page">
       <div className="login-container">
-        <h5 style={{fontSize:"50px"}}></h5>
+        <h5 style={{ fontSize: "50px" }}></h5>
         <h2 className="black-italic-text">Welcome Back</h2>
         <p className="subtext">Continue with Google or enter your details.</p>
         <button className="google-signin-container">
-        <p className="google-signin-text">Sign in with Google</p>
+          <p className="google-signin-text">Sign in with Google</p>
         </button>
         <div className="container">
           <div className="divider"></div>
           <span className="or-text">OR</span>
-        <div className="divider"></div>
+          <div className="divider"></div>
         </div>
-        <form onSubmit={handleSubmit}>
-            <div className="input-container">
-              <FontAwesomeIcon icon={faEnvelope} className="icon" />
-              <input
-                type="text"
-                id="username"
-                placeholder="Email"
-                value={username}
-                onChange={handleUsernameChange}
-                className="input-field"
-              />
-              <hr className={`underline ${username ? 'filled' : ''}`} />
+        <form onSubmit={(e) => e.preventDefault()}>
+          <div className="input-container">
+            <FontAwesomeIcon icon={faEnvelope} className="icon" />
+            <input
+              type="text"
+              id="username"
+              placeholder="Email"
+              value={username}
+              onChange={handleUsernameChange}
+              className="input-field"
+            />
+            <hr className={`underline ${username ? "filled" : ""}`} />
           </div>
           <div className="input-container">
             <FontAwesomeIcon icon={faKey} className="icon" />
@@ -63,13 +90,16 @@ function LoginPage() {
               onChange={handlePasswordChange}
               className="input-field"
             />
-            <hr className={`underline ${username ? 'filled' : ''}`} />
+            <hr className={`underline ${username ? "filled" : ""}`} />
           </div>
           <button className="login-button-container">
-            <button className="login-button">Login</button>
+            <button className="login-button" onClick={handleLogin}>
+              Login
+            </button>
+            <button className="login-button" onClick={handleRegister}>
+              Register
+            </button>
           </button>
-          
-          
         </form>
       </div>
       <div className="gradient-background" />
@@ -87,7 +117,7 @@ function InputFieldsPage() {
   return (
     <div className="password-input-container">
       <input
-        type={passwordVisible ? 'text' : 'password'}
+        type={passwordVisible ? "text" : "password"}
         id="password"
         name="password"
         placeholder="Enter your password"
@@ -101,6 +131,5 @@ function InputFieldsPage() {
     </div>
   );
 }
-
 
 export default LoginPage;
